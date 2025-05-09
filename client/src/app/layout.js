c94be5e0eabb988845aad73579982d6a3f33bc4b
jsoto007 <jsoto7087@gmail.com>
2025-05-09 16:45:33 -0400

    working with no errors

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -18,8 +18,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <div>
-      Hello from Next
-    </div>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
   );
 }
